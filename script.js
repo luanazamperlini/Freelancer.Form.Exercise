6ceff3b1d0237c74219bc0017af73fd6150a6ab2
luana_zamperlini <luazamperlini@gmail.com>
2023-12-17 23:24:39 -0600

    create a table head

--- script.js
+++ script.js
@@ -1,57 +1,38 @@
-/* State */
-// Here, we define variables for the data that our program needs to remember.
-// We call this data "state" because it represents the state of our program.
-
-// TODO: Add support for more colors
-const colors = ["red", "green", "blue"];
-const sizes = ["small", "medium", "large"];
-const maxShapes = 10;
-const shapes = [
-  {
-    color: "red",
-    size: "small",
-  },
-  {
-    color: "yellow",
-    size: "small",
-  },
+const freelancers = [
+  { name: "Dr. Slice", price: 25, occupation: "gardener" },
+  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+  { name: "Prof. Prism", price: 81, occupation: "teacher" },
+  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+  { name: "Prof. Spark", price: 76, occupation: "programmer" },
+  { name: "Dr. Wire", price: 47, occupation: "teacher" },
+  { name: "Prof. Goose", price: 72, occupation: "driver" },
 ];
 
-// `setInterval` will call `addShape` every 1000 milliseconds (1 second)
-// and return an interval ID that we can use to stop the interval later.
-// Calling `clearInterval(addShapeIntervalId)` will stop the interval.
-const addShapeIntervalId = setInterval(addShape, 1000);
+// Select the priceValue to update it's value dynamically
 
-render(); // We call this function once to render the initial state
+const priceValue = document.querySelector("#priceValue");
+//*****************************
+//Create a table header
+function createTableHeader(table) {
+  //Create a thead
+  const thead = document.createElement("thead");
+  //Create a header rows of the table
+  const headerRow = document.createElement("tr");
+  //Create an Array that will store all the headings for the table
+  const headers = ["Name", "Starting Price", "Occupation"];
 
-/**
- * Update the DOM to reflect the current state.
- * The term "render" is often used to describe this process.
- */
-function render() {
-  // Render the squares
-  const squares = document.querySelector("#squares");
-  const squareElements = shapes.map((shape) => {
-    const element = document.createElement("li");
-    element.classList.add(shape.color, shape.size);
-    return element;
-  });
-  squares.replaceChildren(...squareElements);
-
-  // TODO: Render the circles
-}
-
-/**
- * Add a random shape to the `shapes` array
- */
-function addShape() {
-  const color = colors[Math.floor(Math.random() * colors.length)];
-
-  // TODO: Randomize the size of the shape
-
-  shapes.push({ color, size: "small" });
-
-  render();
-
-  // TODO: Stop adding shapes if we've reached the maximum number of shapes
+  //loop over to get every element in the array
+  for (let i = 0; i < headers.length; i++) {
+    //Create a th
+    const th = document.createElement("th");
+    //Add the text content to the th
+    th.innerText = headers[i];
+    //Append the th to the header row
+    headerRow.appendChild(th);
+  }
+  //Append the header row to the thead
+  thead.appendChild(tr);
+  //Append the thead to the table
+  table.appendChild(thead);
 }
